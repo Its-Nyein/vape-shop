350e15befeafc916ce06b328d980315db01def9b
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-01-24 23:14:27 +0630

    update responsive

--- src/components/BestDeals.jsx
+++ src/components/BestDeals.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 import productData from "../data/ProductData";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+// import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 import "./bestDeal.css";
 import { RxDoubleArrowRight } from "react-icons/rx";
 
@@ -10,9 +14,52 @@ function BestDeals() {
       <h1>Best Deals</h1>
       <h3>Just For You</h3>
       <div className="best-deals">
-        {productData.map((product) => (
-          <ProductCard isBestDeal={true} key={product.id} image={product.url} />
-        ))}
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            576: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            992: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+            1200: {
+              slidesPerView: 6,
+              spaceBetween: 20,
+            },
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          // modules={[Pagination]}
+          className="mySwiper"
+        >
+          {/* <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide> */}
+          {productData.map((product) => (
+            <SwiperSlide>
+              <ProductCard
+                isBestDeal={true}
+                key={product.id}
+                image={product.url}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <div className="viewMore">
         <a href="#">
